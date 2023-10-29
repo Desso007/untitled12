90a557f20143fbfd102016b292dd7949768cc564
Desso007 <147561660+Desso007@users.noreply.github.com>
2023-10-29 12:27:41 +0300

    Update Task7.java

--- src/main/java/edu/hw3/Task7.java
+++ src/main/java/edu/hw3/Task7.java
@@ -1,6 +1,6 @@
-package edu.hw3;
 import java.util.Comparator;
 import java.util.TreeMap;
+
 public class Task7 {
     public static void main(String[] args) {
         TreeMap<String, String> tree = new TreeMap<>(new NullSafeStringComparator());
